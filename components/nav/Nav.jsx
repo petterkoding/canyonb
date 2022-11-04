2d70934da9112c74c455f08ec031ab9f79a4bf20
petterkoding <69626923+petterkoding@users.noreply.github.com>
2022-11-04 15:20:27 +0100

    styling navbar

--- components/nav/Nav.jsx
+++ components/nav/Nav.jsx
@@ -3,34 +3,34 @@ import ScreenContainer from '@/ui/ScreenContainer'
 
 const Nav = () => {
   return (
-    <header className="bg-gray-200 h-28">
+    <header className="bg-gray-200 h-auto">
       <nav className="flex flex-col justify-between h-full">
         <ScreenContainer>
-          <div className="flex flex-row items-center justify-between h-full">
+          <div className="flex flex-row items-center justify-between h-full relative pb-2">
 
             <div className="font-bold text-2xl">Logo</div>
 
-              <ul className="flex text-xs text-gray-600 space-x-8">
+              <ul className="flex text-xs text-gray-700 space-x-8">
                 <li>Kommer snart</li>
                 <li>Servicesenter</li>
                 <li>Sykler på lager</li>
                 <li>Finn reservedeler</li>
-                <li>Jobb hos Canyonr</li>
+                <li>Jobb hos Canyon</li>
               </ul>
 
           </div>
 
           <div className="h-full">
 
-            <ul className="flex">
-              <li>road bikes</li>
-              <li>gravel bikes</li>
-              <li>mountain bikes</li>
-              <li>elsykkel</li>
-              <li>hybrid</li>
-              <li>tilbehør</li>
-              <li>outlet</li>
-              <li>service</li>
+            <ul className="flex justify-center uppercase text-sm text-gray-800 h-full cursor-pointer">
+              <li className="hover:bg-white py-2 px-8 hover:text-orange-600">road bikes</li>
+              <li className="hover:bg-white py-2 px-8 hover:text-orange-600">gravel bikes</li>
+              <li className="hover:bg-white py-2 px-8 hover:text-orange-600">mountain bikes</li>
+              <li className="hover:bg-white py-2 px-8 hover:text-orange-600">elsykkel</li>
+              <li className="hover:bg-white py-2 px-8 hover:text-orange-600">hybrid</li>
+              <li className="hover:bg-white py-2 px-8 hover:text-orange-600">tilbehør</li>
+              <li className="hover:bg-white py-2 px-8 hover:text-orange-600">outlet</li>
+              <li className="hover:bg-white py-2 px-8 hover:text-orange-600 font-bold">service</li>
             </ul>
 
 
