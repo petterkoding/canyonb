31ef5d99b0ecf930bf6ebd0baba6f20fe105f47b
petterkoding <69626923+petterkoding@users.noreply.github.com>
2022-11-05 16:49:40 +0100

    displaynames added

--- components/typography/MiniLabel.jsx
+++ components/typography/MiniLabel.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const MiniLabel = ({tag}) => {
+
+  MiniLabel.displayName = "MiniLabel"
+
   return (
     <span className="text-orange-600 text-xs align-top ml-1 font-normal">
       {tag}
--- components/typography/VariableHeading.jsx
+++ components/typography/VariableHeading.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const VariableHeading = ({title, as, styles}) => {
+
+  VariableHeading.displayName = "VariableHeading"
+
   const Heading = as
   return (
     <Heading className={styles}>{title}</Heading>
